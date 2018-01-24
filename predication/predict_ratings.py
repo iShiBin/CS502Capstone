1dd4b83fc1461bdf9e2a277b7fa51868c55d994b
EC2 Default User <ec2-user@ip-172-26-14-215.us-west-2.compute.internal>
2018-01-24 03:43:22 +0000

    tries

--- predication/predict_ratings.py
+++ predication/predict_ratings.py
@@ -9,10 +9,13 @@ pip3 install matplotlib
 
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('agg',warn=False, force=True)
+from matplotlib import pyplot as plt
 from matplotlib import pylab as pl
 from sklearn.neighbors.classification import KNeighborsClassifier
-
-# %matplotlib inline
+from datetime import datetime
+#%matplotlib inline
 
 ccdefault = pd.read_csv('all_ratings.txt')
 
@@ -29,14 +32,20 @@ test = ccd[test_idx == False]
 type(train)
 train.head()
 
-features = ['movie_id', 'user_id']
+features = ['movie_id', 'user_id','date']
 
-n = 2
+# for n in (1,3,5,7,11,13,17,19,23,29,37,41,43,47,53,59)
+for n in range(100, 500, 100):
 
-clf = KNeighborsClassifier(n_neighbors = n)
-clf = clf.fit(train[features], train['rating'])
-preds = clf.predict(test[features])
+    start = datetime.now()
+    clf = KNeighborsClassifier(n_neighbors = n)
+    clf = clf.fit(train[features], train['rating'])
+    preds = clf.predict(test[features])
 
-# accuracy = sklearn.metrics.accuracy_score(np.asarray(clf), np.asarray(preds))
-accuracy = np.where(preds == test['rating'], 1, 0).sum() / float(len(test))*100
-print('Neighbors: %d, Accuracy: %2d%%' % (n, round(accuracy, 2))) #Print Accuracy
+    # accuracy = sklearn.metrics.accuracy_score(np.asarray(clf), np.asarray(preds))
+    accuracy = np.where(preds == test['rating'], 1, 0).sum() / float(len(test))*100
+    end = datetime.now()
+
+    print('Neighbors: %d, Accuracy: %2d%%' % (n, round(accuracy, 2))) #Print Accuracy
+    print('running time:', end - start)
+    print()
