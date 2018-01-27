7dac5ea3514adfa85e5a688f6e87bc5d9622b9c9
Albert Yang <yang.ze@foxmail.com>
2018-01-26 21:43:22 -0500

    cassandra->spark rdd->dataFrame update

--- spark/spark-Cassandra.scala
+++ spark/spark-Cassandra.scala
@@ -5,15 +5,15 @@ import org.apache.spark.sql.cassandra._
 
 //Use the sc.cassandraTable method to view this table as a Spark RDD:
 val rdd = sc.cassandraTable("bittiger", "movie_cs502")
-println(rdd.count)
-println(rdd.first)
+//println(rdd.count)
+//println(rdd.first)
 //rdd.foreach(println)//print the table
 
 
 //Top 10 rated movie in given month
 val sqlContext = new  org.apache.spark.sql.SQLContext(sc)
 import sqlContext.implicits._ //convert implicitly an RDD to DataFrame
-rdd.toDF()//<console>:37: error: value toDF is not a member of com.datastax.spark.connector.rdd.CassandraTableScanRDD[com.datastax.spark.connector.CassandraRow]
+//rdd.toDF()//<console>:37: error: value toDF is not a member of com.datastax.spark.connector.rdd.CassandraTableScanRDD[com.datastax.spark.connector.CassandraRow]
 
 //https://docs.datastax.com/en/datastax_enterprise/4.8/datastax_enterprise/spark/sparkSCcontext.html
 
@@ -24,6 +24,6 @@ firstRow.get[Int]("movie_id")
 //res7: Int = 16879
 
 //load the data in a data frame !!!
-val empDF = spark.read.format("org.apache.spark.sql.cassandra").options(Map("keyspace"->"bittiger" , "table"->"movie_cs502")).load
+val DF = spark.read.format("org.apache.spark.sql.cassandra").options(Map("keyspace"->"bittiger" , "table"->"movie_cs502")).load
 // https://bigdataclassmumbai.com/integrating-apache-spark-apache-cassandra/
-
+top10.map(t=> "movie:" +t(0)).collect().foreach(println)
