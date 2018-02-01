67cfe8bce8028d50fa32e69482be52086630234b
Bin Shi <i@shibin.info>
2018-02-01 13:15:08 -0800

    changed from netflix to movielens dataset

--- ingestion/movie.py
+++ ingestion/movie.py
@@ -1,20 +1,20 @@
 import kafka
-from datetime import datetime
 import time
 
-file_name = '../data/movie_titles.txt'
-kafka_server = 'cs502-kafka-node1:9092'
-topic_name = 'movie_title_year_cs502'
-
+kafka_server = 'node1:9092' # node1: the kafka server name or actual IP address
 producer = kafka.KafkaProducer(bootstrap_servers = kafka_server)
 
+file_name = '../data/movies.csv'
+topic_name = 'movie_title_year_cs502'
+
 with open(file_name, 'r') as data:
+    next(data) # skip the header
     for movie in data:
         first_comma_index = movie.find(',')
         movie_id = movie[:first_comma_index]
-        movie_year_name = movie[first_comma_index + 1:-1]
-        producer.send(topic = topic_name, value = movie_year_name, key= movie_id, timestamp_ms=time.mktime(time.gmtime()))
-#         time.sleep(1)
-
+        movie_info = movie[first_comma_index + 1:-1]
+        print movie_id, movie_info
+        producer.send(topic = topic_name, value = movie_info, key= movie_id, timestamp_ms=time.mktime(time.gmtime()))
+ 
 producer.flush()
 producer.close(timeout=30)
